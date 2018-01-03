a0fcbeb1180c0b61864601cbdb7321ffdfa3516d
revision4 <35070942+revision4@users.noreply.github.com>
2018-01-03 10:12:55 -0500

    Add files via upload

---
+++ rename-computer.ps1
@@ -0,0 +1,124 @@
+﻿# Get the ID and security principal of the current user account
+
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+
+ 
+
+# Get the security principal for the Administrator role
+
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+ 
+
+# Check to see if we are currently running "as Administrator"
+
+if ($myWindowsPrincipal.IsInRole($adminRole))
+
+   {
+
+   # We are running "as Administrator" - so change the title and background color to indicate this
+
+   $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+
+   $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+
+   clear-host
+
+   }
+
+else
+
+   {
+
+   # We are not running "as Administrator" - so relaunch as administrator
+
+   
+
+   # Create a new process object that starts PowerShell
+
+   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+   
+
+   # Specify the current script path and name as a parameter
+
+   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+
+   
+
+   # Indicate that the process should be elevated
+
+   $newProcess.Verb = "runas";
+
+   
+
+   # Start the new process
+
+   [System.Diagnostics.Process]::Start($newProcess);
+
+   
+
+   # Exit from the current, unelevated, process
+
+   exit
+
+   }
+
+ 
+
+# Run your code that needs to be elevated here
+
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form 
+$form.Text = "Rename Computer"
+$form.Size = New-Object System.Drawing.Size(300,200) 
+$form.StartPosition = "CenterScreen"
+
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Point(75,120)
+$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton.Text = "OK"
+$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $OKButton
+$form.Controls.Add($OKButton)
+
+$CancelButton = New-Object System.Windows.Forms.Button
+$CancelButton.Location = New-Object System.Drawing.Point(150,120)
+$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+$CancelButton.Text = "Cancel"
+$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $CancelButton
+$form.Controls.Add($CancelButton)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,20) 
+$label.Size = New-Object System.Drawing.Size(280,20) 
+$label.Text = "Enter the new computer name below:"
+$form.Controls.Add($label) 
+
+$textBox = New-Object System.Windows.Forms.TextBox 
+$textBox.Location = New-Object System.Drawing.Point(10,40) 
+$textBox.Size = New-Object System.Drawing.Size(260,20) 
+$form.Controls.Add($textBox) 
+
+$form.Topmost = $True
+
+$form.Add_Shown({$textBox.Select()})
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $x = $textBox.Text
+    rename-computer -newName $x
+
+}
+
+
+Write-Host -NoNewLine "Press any key to continue..."
+
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
